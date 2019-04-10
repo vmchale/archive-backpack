08cdf1c6cf8a7aac60e97a1fed79afb23444bd0b
Vanessa McHale <vanessa.mchale@iohk.io>
2019-04-10 17:03:11 -0500

    Sort of working

--- archive-libarchive/src/Archive/FFI.hs
+++ archive-libarchive/src/Archive/FFI.hs
@@ -1,12 +1,27 @@
 module Archive.FFI ( Entry
                    , packToFile
                    , unpackFromFile
+                   , readArchiveBytes
+                   , unpackToDir
+                   , unpackFileToDir
                    ) where
 
-import qualified Codec.Archive as FFI
+import qualified Codec.Archive        as FFI
+import qualified Data.ByteString.Lazy as BSL
 
 type Entry = FFI.Entry
 
+packToFile :: Foldable t => FilePath -> t Entry -> IO ()
 packToFile = FFI.entriesToFile
 
+unpackFromFile :: FilePath -> IO [Entry]
 unpackFromFile = FFI.readArchiveFile
+
+readArchiveBytes :: BSL.ByteString -> [Entry]
+readArchiveBytes = FFI.readArchiveBSL
+
+unpackToDir :: FilePath -> BSL.ByteString -> IO ()
+unpackToDir = FFI.unpackToDirLazy
+
+unpackFileToDir :: FilePath -> FilePath -> IO ()
+unpackFileToDir = FFI.unpackArchive
