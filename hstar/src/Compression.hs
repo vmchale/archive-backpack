d023afadec61f8785b21813f33eacd49b6fab2f4
Vanessa McHale <vamchale@gmail.com>
2019-09-15 14:41:10 -0500

    Use lzlib as well

--- hstar/src/Compression.hs
+++ hstar/src/Compression.hs
@@ -6,10 +6,12 @@ module Compression ( compressionByFileExt
 import qualified Codec.Compression.BZip as BZip
 import qualified Codec.Compression.GZip as GZip
 import qualified Codec.Compression.Lzma as Lzma
+import           Codec.Lzip             as Lzip
 import qualified Data.ByteString.Lazy   as BSL
 import           Data.List              (isSuffixOf)
 
 data Compressor = Lzma
+                | Lz
                 | Bz2
                 | GZip
                 | None
@@ -19,6 +21,7 @@ compressionByFileExt fp | ".tgz" `isSuffixOf` fp = GZip
                         | ".tar.bz2" `isSuffixOf` fp = Bz2
                         | ".tar.gz" `isSuffixOf` fp = GZip
                         | ".tar.xz" `isSuffixOf` fp = Lzma
+                        | ".tar.lz" `isSuffixOf` fp = Lz
                         | ".tar" `isSuffixOf` fp = None
                         | otherwise = error "Suffix not supported or invalid."
 
@@ -26,10 +29,12 @@ decompressor :: Compressor -> (BSL.ByteString -> BSL.ByteString)
 decompressor Lzma = Lzma.decompress
 decompressor Bz2  = BZip.decompress
 decompressor GZip = GZip.decompress
+decompressor Lz   = Lzip.decompressStrict . BSL.toStrict
 decompressor None = id
 
 compressor :: Compressor -> (BSL.ByteString -> BSL.ByteString)
 compressor Lzma = Lzma.compress
 compressor Bz2  = BZip.compress
 compressor GZip = GZip.compress
+compressor Lz   = Lzip.compressStrict . BSL.toStrict
 compressor None = id
