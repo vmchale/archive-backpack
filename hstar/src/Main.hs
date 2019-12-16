3f570b687c59e6cc46965070c8526801e68fe921
Vanessa McHale <vamchale@gmail.com>
2019-12-16 12:22:10 -0600

    Add hstar subcommand

--- hstar/src/Main.hs
+++ hstar/src/Main.hs
@@ -5,20 +5,24 @@ import           Compression
 import           Options.Applicative
 
 -- pack a directory/list of files?
-data Command = PackDir FilePath FilePath
-             | Pack [FilePath] FilePath
-             | Unpack FilePath FilePath
+data Command = PackDir !FilePath !FilePath
+             | Pack ![FilePath] !FilePath
+             | Unpack !FilePath !FilePath
+             | PackSrc !FilePath !FilePath
 
 run :: Command -> IO ()
 run (Unpack src dest) =
     let dec = decompressor (compressionByFileExt src)
         in unpackFileToDirAndDecompress dec src dest
-run (PackDir dir tar) =
+run (PackDir dir' tar) =
     let comp = compressor (compressionByFileExt tar)
-        in packFromDirAndCompress comp dir tar
+        in packFromDirAndCompress comp dir' tar
 run (Pack fs tar) =
     let comp = compressor (compressionByFileExt tar)
         in packFromFilesAndCompress comp tar fs
+run (PackSrc dir' tar) =
+    let comp = compressor (compressionByFileExt tar)
+        in packSrcDirAndCompress comp dir' tar
 
 unpack :: Parser Command
 unpack = Unpack
@@ -32,10 +36,21 @@ unpack = Unpack
 
 packDir :: Parser Command
 packDir = PackDir
-    <$> argument str
+    <$> dir
+    <*> archive
+
+packSrc :: Parser Command
+packSrc = PackSrc
+    <$> dir
+    <*> archive
+
+dir :: Parser FilePath
+dir = argument str
         (metavar "DIR"
         <> help "Directory to pack up")
-    <*> argument str
+
+archive :: Parser FilePath
+archive = argument str
         (metavar "ARCHIVE"
         <> help "File to pack it to")
 
@@ -46,15 +61,14 @@ pack = Pack
         <> long "file"
         <> short 'f'
         <> help "File to add to archive"))
-    <*> argument str
-        (metavar "ARCHIVE"
-        <> help "File to pack into")
+    <*> archive
 
 cmd :: Parser Command
 cmd = hsubparser
     (command "unpack" (info unpack (progDesc "Unpack an archive"))
     <> command "pack-dir" (info packDir (progDesc "Pack a directory's contents into an archive"))
     <> command "pack" (info pack (progDesc "Pack an archive from a list of files"))
+    <> command "pack-src" (info packSrc (progDesc "Pack up a source directory, ignoring version control and artifact directories"))
     )
 
 topLevel :: ParserInfo Command
