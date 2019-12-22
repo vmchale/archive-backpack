19bc3de8217aa3e1d9eb1926198643d31f617339
Vanessa McHale <vamchale@gmail.com>
2019-12-21 18:42:02 -0600

    .txz suffix

--- hstar/src/Compression.hs
+++ hstar/src/Compression.hs
@@ -21,6 +21,7 @@ compressionByFileExt fp | ".tgz" `isSuffixOf` fp = GZip
                         | ".tar.bz2" `isSuffixOf` fp = Bz2
                         | ".tar.gz" `isSuffixOf` fp = GZip
                         | ".tar.xz" `isSuffixOf` fp = Lzma
+                        | ".txz" `isSuffixOf` fp = Lzma
                         | ".tar.lz" `isSuffixOf` fp = Lz
                         | ".tar" `isSuffixOf` fp = None
                         | otherwise = error "Suffix not supported or invalid."
