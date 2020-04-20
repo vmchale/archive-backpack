e70119ba26cb6d3054d010baf9c558933ff12661
Vanessa McHale <vamchale@gmail.com>
2020-04-20 09:34:58 -0500

    make robust

---
+++ snappy-lazy/CHANGELOG.md
@@ -0,0 +1,5 @@
+# snappy-lazy
+
+## 0.1.0.0
+
+Initial release
---
+++ snappy-lazy/src/Codec/Compression/Snappy/BSL.hs
@@ -0,0 +1,35 @@
+module Codec.Compression.Snappy.BSL ( compress
+                                    , decompress
+                                    ) where
+
+import qualified Codec.Compression.Snappy         as Snappy
+import qualified Codec.Compression.Snappy.Framing as Snappy
+import           Data.Binary                      (decodeOrFail, encode)
+import qualified Data.ByteString.Lazy             as BSL
+
+
+decompress :: BSL.ByteString -> BSL.ByteString
+decompress = BSL.fromChunks . loop
+    where loop bs =
+            let (res, _, chunk) = yeet $ decodeOrFail bs
+                in if BSL.null res
+                    then [extractUncompressed chunk]
+                    else extractUncompressed chunk : loop res
+          yeet = either (error.show) id
+          extractUncompressed (Snappy.Compressed _ d)   = Snappy.decompress d
+          extractUncompressed (Snappy.Uncompressed _ x) = x
+          extractUncompressed Snappy.StreamIdentifier   = mempty
+          extractUncompressed Snappy.Skippable{}        = mempty
+          extractUncompressed _                         = error "Expected Uncompressed{}, Skippable{} or StreamIdentifier; possible corrupt stream"
+
+compress :: BSL.ByteString -> BSL.ByteString
+compress = (Snappy.streamIdentifier <>) . loop
+    where loop bsl =
+            let (chunk, res) = Snappy.encode bsl
+                in case res of
+                    Just x  -> extractCompressed chunk <> loop x
+                    Nothing -> extractCompressed chunk
+          extractCompressed c@Snappy.Compressed{}   = encode c
+          extractCompressed c@Snappy.Uncompressed{} = encode c
+          -- see: http://hackage.haskell.org/package/snappy-framing-0.1.2/docs/Codec-Compression-Snappy-Framing.html#v:encode
+          extractCompressed _                       = error "Expected Compressed{}; possible corrupt stream"
