705feeba8ad6090bd363409e8e9cd8042f458e8a
Vanessa McHale <vamchale@gmail.com>
2020-08-14 17:43:59 -0500

    Add lint for potentially insecure harlinks

--- hstar/src/Lint.hs
+++ hstar/src/Lint.hs
@@ -5,11 +5,14 @@ module Lint ( lintEntry
 import           Codec.Archive
 import           Control.Exception (Exception, throwIO)
 import           Control.Monad     (when)
+import           Data.List         (isPrefixOf)
 
-newtype LintException = SelfHardlink FilePath
+data LintException = SelfHardlink FilePath
+                   | InsecureLink FilePath
 
 instance Show LintException where
     show (SelfHardlink fp) = "Entry " ++ fp ++ " is a hardlink pointing to itself."
+    show (InsecureLink fp) = "Entry " ++ fp ++ " is an insecure link"
 
 instance Exception LintException
 
@@ -17,8 +20,18 @@ selfLink :: Eq fp => Entry fp e -> Bool
 selfLink (Entry fp (Hardlink fp') _ _ _) = (fp == fp')
 selfLink _                               = False
 
+insecurePath :: FilePath -> Bool
+insecurePath fp | "/" `isPrefixOf` fp = True
+                | otherwise = False
+
 lintEntry :: Entry FilePath e -> IO ()
 lintEntry (Entry fp (Hardlink fp') _ _ _) =
     when (fp == fp') $
         throwIO (SelfHardlink fp)
+lintEntry (Entry fp (Hardlink fp') _ _ _) =
+    when (insecurePath fp') $
+        print (InsecureLink fp)
+lintEntry (Entry fp (Symlink fp' _) _ _ _) =
+    when (insecurePath fp') $
+        print (InsecureLink fp)
 lintEntry _ = pure ()
